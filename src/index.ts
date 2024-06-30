da66e3b38ac1819a876d01c2f4793ff498a00a04
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-06-30 19:08:42 +0530

    Configured winston logging

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import fastify from 'fastify';
 
-const server = fastify();
+import logger from './config/logger.config';
 
+const server = fastify();
 server.get('/ping', async (request, reply) => {
     return 'pong\n';
 });
 
-server.listen({ port: 8080 }, (err, address) => {
-    if (err) {
-        console.error(err);
+server.listen({ port: 8080 }, (error, address) => {
+    if (error) {
+        logger.error(`Error Starting Server: ${error}`);
         process.exit(1);
     }
     console.log(`Server listening at ${address}`);
