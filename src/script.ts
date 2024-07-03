022d8449006121ff209403ef6aab17ea837de04a
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-07-03 20:38:25 +0530

    added k6 run script

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,4 @@
-// npm i @types/k6 --> k6
+// npm i @types/k6 --> k6 run src/script.ts
 // sudo npm i -g pm2 --> pm2 start script.ts --interpreter ts-node -i max --> It Means we are running max number of cors of our machine
 import http from 'k6/http';
 import { sleep } from 'k6';
@@ -17,11 +17,11 @@ export let options = {
         { duration: '15s', target: 100 },
         { duration: '10s', target: 600 },
         { duration: '20s', target: 500 }
-    ]
+    ],
 };
 
 export default function () {
-    let response = http.get('http://127.0.0.1:8080/api/v1/test/ping');
+    let response = http.get('http://127.0.0.1:3000/api/v1/test/ping');
     let success = response.status === 200;
     errorRate.add(!success);
     sleep(1 / 100);
